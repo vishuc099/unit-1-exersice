57a5ecf735598aa85a078935906eb505dc06723e
vishuc099 <vishvachhag51@gmail.com>
2025-07-11 23:59:10 -0400

    Add files via upload

---
+++ Sum of two variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+    cout << "Enter Number 1: ";
+    cin >> a;
+    cout << "Enter Number 2: ";
+    cin >> b;
+    sum = a + b;
+    cout << "Sum is: " << sum << endl;
+    return 0;
+}
---
+++ swap two nimbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, temp;
+    cout << "Enter Value Of A: ";
+    cin >> a;
+    cout << "Enter Value Of B: ";
+    cin >> b;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "After swap, A = " << a << ", B = " << b << endl;
+    return 0;
+}
